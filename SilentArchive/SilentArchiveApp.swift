2dc497a1c8ec7fa126ca53f943ec65b535271192
loadingForMom <sashavasilev56753@icloud.com>
2026-01-17 17:50:23 +0300

    Initial Commit

---
+++ SilentArchive/SilentArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentArchiveApp.swift
+//  SilentArchive
+//
+//  Created by Sasha on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SilentArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
